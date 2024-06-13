aaebe029e8d7f15ace22b48893ae90e55cf6e9ef
Douglas Walker <douglas.walker@kaizenreporting.com>
2024-06-13 13:52:49 +0100

    add react logic

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    alert("React is here!");
+  }, []);
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
